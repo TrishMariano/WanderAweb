26972e4b9aaed543b0ad2ea083bd38ff3edb7fa0
Unknown <kennethjiepadasas@gmail.com>
2018-07-21 18:21:24 +0800

    auth and manage Business

---
+++ public/dashboard/app.js
@@ -0,0 +1,12 @@
+function manageBusiness(){
+  console.log("test");
+  ReactDOM.render(
+    <div className = "list-group">Manage Business</div>,document.getElementById("mainContainer")
+  );
+}
+// 
+// class Business extends React.component{
+//   return(
+//     <button type="button" class="list-group-item list-group-item-action">{this.props.businessname}</button>
+//   );
+// }
